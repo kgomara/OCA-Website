c8f8441aca41b140157aec234b07eaa157258a74
Kevin OMara <kgomara@yahoo.com>
2016-11-29 22:50:20 -0600

    Add initial tech library components

---
+++ WebContent/js/library.js
@@ -0,0 +1,51 @@
+var libraryData = [
+	{
+		"type":			"refcard",
+		"fileName":		"agile_adoption-reducecost.pdf",
+		"title":		"Agile Adoption: Reducing Cost",
+		"description":	"Some of the costs of software development are associated with man hours needed to build the system, others with cost of maintenance over time, and yet others include hardware as well as software platform costs. Practices that educe any or all of these costs without sacrificing quality reduce the overall cost of the system.",
+		"tags": [
+			"agile"
+		]
+	},
+	{
+		"type":			"refcard",
+		"fileName":		"agile_adoption.pdf",
+		"title":		"Agile Adoption: Decreasing Time to Market",
+		"description":	"This Refcard is focused on helping you evaluate and choose the practices for your team or organization when getting to market faster is of prime importance. Instead of focusing on entire methods such as Scrum and XP, we will talk about the practices that are the building blocks of these methods such as iterations and automated developer tests.",
+		"tags": [
+			"agile"
+		]
+	},
+	{
+		"type":			"refcard",
+		"fileName":		"agile_improve_quality.pdf",
+		"title":		"Agile Adoption: Improving Software Quality",
+		"description":	"The vast majority of software projects suffer from a steady degradation of design quality and it becomes more and more difficult to maintain the software with the same level of quality. As the software ages it calcifies and becomes harder and harder to maintain. In some cases it becomes too expensive to maintain and so it the software is put to rest and rewritten. In others, the software is released with a steadily increasing number of defects. Both of these common situations are deeply unsatisfying, but there is another way.",
+		"tags": [
+			"agile"
+		]
+	},
+	{
+		"type":			"refcard",
+		"fileName":		"android.pdf",
+		"title":		"Code Gems for Android Developers",
+		"description":	"<p>Android is a stack of operating system, middle ware and applications developed by Google that is dedicated to mobile devices. Android relies on Linux kernel 2.6 for core services.</p><p>The Android platform provides API and Tools to develop applications using Java Platform. It features Dalvik Virtual Machine, SQLite, an integrated browser, application framework, as well as various media and hardware sources.</p>",
+		"tags": [
+			"android", "code tips and tricks"
+		]
+	},
+	{
+		"type":			"refcard",
+		"fileName":		"ApacheHTTPD_1.pdf",
+		"title":		"Essential Apache HTTP Server",
+		"description":	"Apache httpd is the world’s most popular web server, running more than half of all the websites on the planet. It runs on every modern operating system, and has numerous modules – both included with the base distribution, and third-party modules – which provide a wide range of optional functionality, for every type of website.",
+		"tags": [
+			"apache", "http", "server"
+		]
+	}
+];
+
+var tags = [
+	"agile", "android", "code tips and tricks", "apache", "http", "server"
+];
